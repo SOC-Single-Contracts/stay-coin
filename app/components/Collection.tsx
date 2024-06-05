421fdca7e80159802c7ccfe7986bcd31a5f74975
ansariwaseeq <ansariwaseeq5@gmail.com>
2024-06-05 19:06:53 +0500

    heroborder ribbon fix

--- app/components/Collection.tsx
+++ app/components/Collection.tsx
@@ -17,42 +17,42 @@ const Collection = () => {
 
             <div className="relative w-90  ">
 
-                <div className="absolute inset-0 grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 opacity-20 gradient-to-t z">
-                    <div className="p-4  ">
-                        <Image src={confetti} alt="confetti" className="w-full h-full object-cover" />
+                <div className="absolute inset-0 grid grid-cols-4  opacity-20 gradient-to-t z">
+                    <div className="p-2">
+                        <Image src={confetti} alt="confetti" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={pepe} alt="pepe" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={pepe} alt="pepe" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={island} alt="island" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={island} alt="island" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={scifi} alt="scifi" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={scifi} alt="scifi" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={viking} alt="viking" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={viking} alt="viking" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={timelineImage} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={timelineImage} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={timelineImage} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={timelineImage} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={viking} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={viking} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={scifi} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={scifi} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={island} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={island} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={pepe} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={pepe} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
-                    <div className="p-4 {/*sm:w-[200px] h-[200px]*/}">
-                        <Image src={confetti} alt="timeline" className="w-full h-full object-cover" />
+                    <div className="p-2">
+                        <Image src={confetti} alt="timeline" className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-[223px] lg:h-[223px] object-cover" />
                     </div>
                 </div>
 
@@ -69,12 +69,12 @@ const Collection = () => {
                             Swab aye red ahoy chandler. Jack <br className="lg:hidden" />
                             scourge driver boat fer of yarr red <br className="lg:hidden" />
                             tales. Just gang away hang chase jjb arrgh gold
-                            lanyard parre. Scurvy lateen warrp chanddlerr gar boat 
+                            lanyard parre. Scurvy lateen warrp chanddlerr gar boat
                             boatswait men.
                         </p>
 
                         {/* Buttons */}
-                        <div className="flex flex-col sm:flex-row gap-2 lg:gap-1 justify-center">
+                        <div className="flex flex-col sm:flex-row gap-3 lg:gap-2 justify-center">
                             <button
                                 className="bg-red-800 lg:ml-16 py-4 px-14 rounded-full text-lg font-medium lg:text-xl text-white"
                                 onClick={() => console.log('View Collection')}
@@ -97,28 +97,28 @@ const Collection = () => {
                     <p className="font-gilroy text-2xl lg:text-4xl text-center mb-4 sm:mb-10" style={{ fontWeight: 700, lineHeight: "40px" }}>
                         We Are Listed On The <br className="lg:hidden" /> Following Exchanges
                     </p>
-  
 
-          <p className="font-inter text-base lg:text-lg text-center mb-10" style={{ lineHeight: "25px" }}>
-            Swab aye red ahoy chandler. Jack <br className="lg:hidden" /> scourge driver boat fer of yarr red <br className="lg:hidden" /> tales.
-          </p>
-        </div>
-        <div className="flex flex-col lg:flex-row justify-center gap-5 my-10">
-          {[1, 2, 3, 4].map((index) => (
-            <div key={index} className="w-full lg:w-auto cursor-pointer">
-              <div style={{ background: "rgba(225, 49, 34, 0.08)" }} className="w-[80%] mx-auto lg:w-56 flex flex-col gap-1 justify-start items-center border border-primary rounded-3xl p-5">
-                <Image src={horse} alt="" />
-                <div className="flex items-center">
-                  <p >Uniswap</p>
-                  <FaArrowRightLong className="text-primary" />
+
+                    <p className="font-inter text-base lg:text-lg text-center mb-10" style={{ lineHeight: "25px" }}>
+                        Swab aye red ahoy chandler. Jack <br className="lg:hidden" /> scourge driver boat fer of yarr red <br className="lg:hidden" /> tales.
+                    </p>
                 </div>
-              </div>
+                <div className="flex flex-col lg:flex-row justify-center gap-5 my-10">
+                    {[1, 2, 3, 4].map((index) => (
+                        <div key={index} className="w-full lg:w-auto cursor-pointer">
+                            <div style={{ background: "rgba(225, 49, 34, 0.08)" }} className="w-[80%] mx-auto lg:w-56 flex flex-col gap-1 justify-start items-center border border-primary rounded-3xl p-5">
+                                <Image src={horse} alt="" />
+                                <div className="flex items-center">
+                                    <p >Uniswap</p>
+                                    <FaArrowRightLong className="text-primary" />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div >
-  );
+        </div >
+    );
 };
 
 export default Collection;
