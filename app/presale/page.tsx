d2f066424d6fbea2ac0722fc8bce7ba1df3458f0
waseeqansari-soc <waseeq.ansari@soc-solution.com>
2024-06-07 11:54:06 +0500

    Create page.tsx

---
+++ app/presale/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+import Presale from '../components/Presale'
+export default function Home() {
+  return (
+    <div className="bg-black text-white">
+    <Presale />
+    </div>
+  );
+}
